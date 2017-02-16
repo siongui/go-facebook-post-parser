d0b7fe0395e5ba0de402c6da697234d0321d78a3
siongui <siongui@gmail.com>
2017-02-16 23:49:04 +0800

    add test code

--- fb_test.go
+++ fb_test.go
@@ -6,11 +6,25 @@ import (
 
 func TestParse(t *testing.T) {
 	//url := "https://www.facebook.com/jayasaro.panyaprateep.org/photos/a.318290164946343.68815.318196051622421/1119567364818615/?type=3"
-	url := "https://www.facebook.com/jayasaro.panyaprateep.org/posts/1095007907274561:0"
+	//url := "https://www.facebook.com/jayasaro.panyaprateep.org/posts/1095007907274561:0"
 	//url := "https://www.facebook.com/jayasaro.panyaprateep.org/photos/a.318290164946343.68815.318196051622421/1119561951485823/?type=3"
-	result, err := Parse(url)
+	//result, err := Parse(url)
+
+	post, err := ParsePost(testhtml1, "")
 	if err != nil {
 		t.Error(err)
 	}
-	t.Log(result)
+	t.Log(post)
+
+	post, err = ParsePost(testhtml2, "")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(post)
+
+	post, err = ParsePost(testhtml3, "")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(post)
 }
